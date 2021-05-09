8a3200f30069f15a8aab7edcf3403fd457c6c85f
lxh6688 <1272662790@qq.comom>
2021-05-09 18:51:16 +0800

    创建 button group

---
+++ src/button-group.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="s-button-group">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['name']
+}
+</script>
+
+<style lang="scss">
+  .s-button-group{ 
+    display: inline-flex; 
+    vertical-align: middle;
+    > .s-button {
+      border-radius: 0;
+     margin-left: -1px;
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
