93d72195fae426bc0d874ad8ccca4075e451803f
lxh6688 <1272552790@qq.comm>
2021-05-18 20:51:45 +0800

    添加简单动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,16 +1,30 @@
 <template>
-  <div class="sider">
+  <div class="sider" v-if="visible">
     <slot></slot>
+    <button @click="visible = false">close</button>
   </div>
 </template>
 <script>
 export default {
-  name: 'starsSider'
+  name: 'starsSider',
+  data(){
+    return {
+      visible: true
+    }
+  },
+  methods: {
+    
+  }
 }
 </script>
 
 <style lang="scss" scoped>
   .sider{
-    
+    position: relative;
+    > button{
+      position: absolute;
+      top: 0;
+      right: 0;
+    }
   }
 </style>
